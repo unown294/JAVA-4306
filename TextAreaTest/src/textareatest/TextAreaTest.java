0ab02ee3724b168f42d2194240ee26a7e03bd668
Dalton <trainguy294@gmail.com>
2020-02-20 09:07:02 -0600

    In-Class Notes/Assignments
    Both are GUI Interface systems

---
+++ TextAreaTest/src/textareatest/TextAreaTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textareatest;
+
+/**
+ *
+ * @author Dalton Redman <dalton.redman@mavs.uta.com>
+ */
+public class TextAreaTest {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
